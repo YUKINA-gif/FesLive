a66acb835d7d88248f65aac2c9994f30bd7942ea
Yukina Nakanishi <yukina-nakanishi@outlook.jp>
2021-07-03 21:45:05 +0900

    スワイプ追加

---
+++ src/components/Swipe.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="swipe">
+    <carousel
+    :per-page="4">
+      <slide v-for="image in images" :key="image.id">
+        <img :src="image.image" alt="イベント画像" class="image swipe_image">
+      </slide>
+    </carousel>
+  </div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+export default {
+  data() {
+    return {
+      images:[
+        {id:1,image:"favicon.ico"},
+        {id:2,image:"favicon.ico"},
+        {id:3,image:"favicon.ico"},
+        {id:4,image:"favicon.ico"},
+        {id:5,image:"favicon.ico"},
+        {id:6,image:"favicon.ico"},
+        {id:7,image:"favicon.ico"},
+        {id:8,image:"favicon.ico"},
+      ],
+    }
+  },
+  components: {
+    Carousel,
+    Slide
+  }
+}
+</script>
+
+<style scoped>
+.swipe_image{
+  width: 90px;
+  border: 1px solid #ccc;
+  border-radius: 50px;
+}
+.flex{
+  flex-shrink: 0;
+}
+
+</style>
--- src/views/TimeLine.vue
+++ src/views/TimeLine.vue
@@ -1,22 +1,61 @@
 <template>
   <div class="time_line">
+    <!-- メインイメージ -->
+    <h1 class="main_title">FesLive</h1>
     <img :src="image" alt="メインイメージ" class="image main_image">
+    <!-- イベントスワイプ -->
+    <div class="title">
+      <h2>イベント一覧</h2>
+    </div>
+    <Swipe />
+    <div class="title">
+      <h2>ツイート一覧</h2>
+    </div>
+    <Timeline :id="tweet_id" sourceType="list" />
   </div>
 </template>
 
 <script>
+import Swipe from '../components/Swipe.vue';
+import { Timeline } from 'vue-tweet-embed';
 export default {
   data() {
     return {
-      image: require("../assets/live.jpg")
+      image: require("../assets/fesimage.jpg"),
+      tweet_id:"9hp_be"
     }
   },
+  components:{
+    Swipe,
+    Timeline
+  }
 }
 </script>
 
 <style scoped>
+/* ===============
+      メイン
+=============== */
+  .main_title{
+    position: absolute;
+    top: 30px;
+    left: 20px;
+    font-size: 35px;
+    font-family: fantasy;
+    color: #fff;
+    z-index: 999;
+  }
   .main_image{
     width: 100%;
-    height: 300px;
+    height: 150px;
+    opacity: 0.8;
+  }
+  .title{
+    color: gray;
+    border-bottom: 2px solid rgb(194, 104, 194);
+    border-left: 5px solid rgb(194, 104, 194);
+    width: 120px;
+    margin: 10px;
+    padding: 0 0 2px 2px;
   }
 </style>
